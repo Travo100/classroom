c88bae8db7622961405da6e9f5aa520f62034d07
Travo100 <Travo100@gmail.com>
2018-08-16 11:37:58 -0700

    Init commit of the classroom

---
+++ src/components/Student/Student.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Student = (props) => (
+    <div className="student">
+        <p>Id: {props.id}</p>
+        <p>Name: {props.name}</p>
+        <p>Favorite Subject: {props.favoriteSubject}</p>
+        <button onClick={() => props.rollCall(props.name, props.favoriteSubject)}>Call on student</button>
+    </div>
+);
+
+export default Student;
