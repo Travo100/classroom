96b5da182267ab83dbab947c5daae3f1d4f381c0
Travo100 <Travo100@gmail.com>
2018-08-16 12:49:03 -0700

    Added this.setState

--- src/components/Classroom/Classroom.js
+++ src/components/Classroom/Classroom.js
@@ -6,17 +6,24 @@ import Student from "../Student";
 class Classroom extends Component {
 
     state = {
-        students: students
+        students: students,
+        studentNameCalled: ""
     };
 
     rollCall = (name, favSubject) => {
         alert(`Hello my name is ${name} and my favorite subject is ${favSubject}`);
+        this.setState({
+            studentNameCalled: name
+        });
     }
 
     render() {
         return (
             <div>
                 <h1>Classroom</h1>
+                <div>
+                    <p>The following student was called on {this.state.studentNameCalled}</p>
+                </div>
                 {this.state.students.map(student => (
                     <Student
                         id={student.id}
