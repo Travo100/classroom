9dcf7a3cedacfe2d7aebe960d1e9438fcdcfa8f4
Travo100 <Travo100@gmail.com>
2018-08-21 13:18:17 -0700

    pushing up student history

--- src/components/Classroom/Classroom.js
+++ src/components/Classroom/Classroom.js
@@ -7,13 +7,18 @@ class Classroom extends Component {
 
     state = {
         students: students,
-        studentNameCalled: ""
+        studentNameCalled: "",
+        studentsArray: []
     };
 
     rollCall = (name, favSubject) => {
         alert(`Hello my name is ${name} and my favorite subject is ${favSubject}`);
+        
+        // making a new array with the student named called myStudents 
+        let myStudents = [...this.state.studentsArray, name];
         this.setState({
-            studentNameCalled: name
+            studentNameCalled: name,
+            studentsArray: myStudents //setting the studentsArray to the new myStudents array
         });
     }
 
@@ -26,12 +31,21 @@ class Classroom extends Component {
                 </div>
                 {this.state.students.map(student => (
                     <Student
+                        key={student.id}
                         id={student.id}
                         name={student.name}
                         favoriteSubject={student.favoriteSubject}
                         rollCall = {this.rollCall}
                     />
                 ))}
+                <div className="students-history">
+                    <h2>History of who we called on</h2>
+                    <ol>
+                        {this.state.studentsArray.map(student => (
+                            <li>{student}</li>
+                        ))}
+                    </ol>
+                </div>
             </div>
         )
     }
